c7e5cdf1b3a05316ea4a2e053651b9ed6d5ed365
Kristján Oddsson <koddsson@gmail.com>
2019-11-22 11:38:55 +0000

    move early exit on same input outside of debounced function

--- src/index.js
+++ src/index.js
@@ -111,6 +111,19 @@ function setLoadingState(event: Event) {
     return
   }
 
+  const body = new FormData()
+  body.append('authenticity_token', csrf)
+  body.append('value', input.value)
+
+  const id = body.entries ? [...body.entries()].sort().toString() : null
+  if (id && id === state.previousValue) {
+    if (autoCheckElement.required) {
+      input.setCustomValidity('')
+    }
+    return
+  }
+  state.previousValue = id
+
   let message = 'Verifying…'
   const setValidity = text => (message = text)
   input.dispatchEvent(
@@ -168,10 +181,6 @@ async function check(autoCheckElement: AutoCheckElement) {
   const body = new FormData()
   body.append('authenticity_token', csrf)
   body.append('value', input.value)
-
-  const id = body.entries ? [...body.entries()].sort().toString() : null
-  if (id && id === state.previousValue) return
-  state.previousValue = id
 
   if (!input.value.trim()) {
     if (autoCheckElement.required) {
